9594c817731e93b1e5b268b7029f4c9c337b7d89
James <beardyjames@live.co.uk>
2012-08-26 16:46:16 +0100

    Remove depenedency on fake plugin for player name prompt

--- src/main/java/me/tehbeard/vocalise/prompts/input/InputPlayerNamePrompt.java
+++ src/main/java/me/tehbeard/vocalise/prompts/input/InputPlayerNamePrompt.java
@@ -4,14 +4,16 @@ import me.tehbeard.vocalise.parser.ConfigurablePrompt;
 import me.tehbeard.vocalise.parser.PromptBuilder;
 import me.tehbeard.vocalise.parser.PromptTag;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.conversations.ConversationContext;
 import org.bukkit.conversations.PlayerNamePrompt;
 import org.bukkit.conversations.Prompt;
+import org.bukkit.conversations.ValidatingPrompt;
 import org.bukkit.entity.Player;
 
 @PromptTag(tag = "inpply")
-public class InputPlayerNamePrompt extends PlayerNamePrompt implements ConfigurablePrompt {
+public class InputPlayerNamePrompt extends ValidatingPrompt implements ConfigurablePrompt {
 
     private String input;
     private String session;
@@ -22,7 +24,6 @@ public class InputPlayerNamePrompt extends PlayerNamePrompt implements Configura
         this("","");
     }
     public InputPlayerNamePrompt(String input,String session){
-        super(new FakePlugin());
         this.input = input;
         this.session = session;
     }
@@ -37,8 +38,8 @@ public class InputPlayerNamePrompt extends PlayerNamePrompt implements Configura
 
     @Override
     protected Prompt acceptValidatedInput(ConversationContext context,
-            Player input) {
-        context.setSessionData(session, input);
+            String input) {
+        context.setSessionData(session, Bukkit.getPlayer(input));
         return prompt;
     }
 
@@ -48,6 +49,11 @@ public class InputPlayerNamePrompt extends PlayerNamePrompt implements Configura
         builder.makePromptRef(config.getString("id"),this);
         setPrompt(config.isString("next") ? builder.locatePromptById(config.getString("next")) : builder.generatePrompt(config.getConfigurationSection("next")));
         
+    }
+    @Override
+    protected boolean isInputValid(ConversationContext context, String input) {
+        
+        return Bukkit.getPlayer(input) != null;
     }
 
 }
